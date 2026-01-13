5b151c7a717e90becac6c6606adb1d73597993e2
Battisti-Daniel <danielbattisti@outlook.com.br>
2026-01-13 09:01:54 -0300

    BCrypt Password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$./7qGPb8w0NfAzIgake/aOgU6PfzQn1K8yaEDZDxIxNqj9LC3BekS');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$./7qGPb8w0NfAzIgake/aOgU6PfzQn1K8yaEDZDxIxNqj9LC3BekS');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
